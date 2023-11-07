4dae3cd7ad33658e634055b00bcbaaa780c395c8
hello <22>
2023-11-07 23:29:15 +0800

    Initial commit

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,184 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.awt.*;
+import java.util.Iterator;
+
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
+    Texture img;
+    Texture rainImg;
+    Texture bucketImg;
+    Texture back;
+    private int score;
+    private BitmapFont font;
+    // 相机和 SpriteBatch
+    private OrthographicCamera camera;
+    SpriteBatch batch;
+    //图形
+    private Rectangle bucket;
+    private Array<Rectangle> rain;
+    private long lastDropRainTime;
+    private boolean isStart;
+    private ApplicationAdapter adapter;
+
+    @Override
+    public void create() {
+        Gdx.input.setInputProcessor(this);
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        rainImg = new Texture("photo/Rain.png");
+        bucketImg = new Texture("photo/bucket.jpg");
+        back = new Texture("photo/back.png");
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1200, 960);
+        score = 0;
+        bucket = new Rectangle();
+        bucket.x = 1200 / 2 - 64 / 2;
+        bucket.y = 20;
+        bucket.width = 64;
+        bucket.height = 64;
+
+        isStart = false;
+        adapter = new MyGdxGame();
+
+        font = new BitmapFont();
+        font.setColor(0.25f, 0.8f, 0.16f, 0.9f);
+        font.getData().setScale(3);
+
+        rain = new Array<>();
+        rainDrop();
+    }
+
+    @Override
+    public void render() {
+        //判断是否开始
+        if (!isStart) {
+            //渲染
+            ScreenUtils.clear(1, 1, 1, 1);
+            //Gdx.gl.glClearColor(1, 1, 1, 1);
+            //Gdx.gl.glClear(GL20.GL_STENCIL_VALUE_MASK);
+            camera.update();
+            batch.setProjectionMatrix(camera.combined);
+
+            batch.begin();
+
+            batch.draw(back, 0, 0, 1200, 960);
+            batch.draw(bucketImg, bucket.x, bucket.y);
+            for (Rectangle rainD :
+                    rain) {
+                batch.draw(rainImg, rainD.x, rainD.y);
+            }
+
+            //雨滴下落
+            if (TimeUtils.nanoTime() - lastDropRainTime > 500000000) rainDrop();
+            for (Iterator<Rectangle> iter = rain.iterator(); iter.hasNext(); ) {
+                Rectangle nextDrop = iter.next();
+                nextDrop.y -= 200 * Gdx.graphics.getDeltaTime();
+                if (nextDrop.y < 0) iter.remove();
+                //接住雨滴
+                if (nextDrop.overlaps(bucket)) {
+                    score += 10;
+                    iter.remove();
+                }
+                font.draw(batch, "SCORE: " + score, 160, 766);
+            }
+            batch.end();
+            //鼠标控制
+            if (Gdx.input.isTouched()) {
+                final Vector3 touch = new Vector3();
+                touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+                camera.unproject(touch);
+                bucket.x = (int) (touch.x - 64 / 2);
+            }
+            //键盘控制
+            if (Gdx.input.isKeyPressed(Input.Keys.A)) bucket.x -= 500 * Gdx.graphics.getDeltaTime();
+            if (Gdx.input.isKeyPressed(Input.Keys.D)) bucket.x += 500 * Gdx.graphics.getDeltaTime();
+            //限制范围
+            if (bucket.x < 0) bucket.x = 0;
+            if (bucket.x > 1200) bucket.x = 1200 - 64 * 2;
+        }
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+        bucketImg.dispose();
+        rainImg.dispose();
+        back.dispose();
+    }
+
+    private void rainDrop() {
+        Rectangle rainDrop = new Rectangle();
+        rainDrop.x = MathUtils.random(0, 1200 - 64);
+        rainDrop.y = 800;
+        rainDrop.height = 64;
+        rainDrop.width = 64;
+        rain.add(rainDrop);
+        lastDropRainTime = TimeUtils.nanoTime();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if(keycode == Input.Keys.SPACE){
+            isStart = !isStart;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+
+}
